410a1de63b1e4f1ab4ae1362de69c720df31c10d
Felipe Farnetti <Felipefarnetti@gmail.com>
2023-05-11 09:56:28 +0200

    all routes working - 11-05 - 10h00

--- routes/marvel.js
+++ routes/marvel.js
@@ -4,6 +4,41 @@ const router = express.Router();
 const axios = require("axios");
 
 ///////////////////////////////////////////
+/////////   CHARACTERRS PART ROUTES ////////////
+///////////////////////////////////////////
+
+router.get("/characters", async (req, res) => {
+  try {
+    // Query filters:  name, limit and skip -
+    const name = req.query.name;
+    const limit = req.query.limit || 100;
+    const skip = req.query.skip || 0;
+
+    // Request Characters from API
+    const response = await axios.get(
+      `https://lereacteur-marvel-api.herokuapp.com/characters?apiKey=${process.env.MARVEL_API}&limit${limit}&skip${skip}&name${name}`
+    );
+    const characters = response.data;
+    res.status(200).json(characters);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+router.get("/character/:characterId", async (req, res) => {
+  try {
+    // Request infos of a specific character
+    const response = await axios.get(
+      `https://lereacteur-marvel-api.herokuapp.com/character/${req.params.characterId}?apiKey=${process.env.MARVEL_API}`
+    );
+    const characterId = response.data;
+    res.status(200).json(characterId);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+///////////////////////////////////////////
 /////////   COMICS PART ROUTES ////////////
 ///////////////////////////////////////////
 
@@ -51,41 +86,6 @@ router.get("/comic/:comicId", async (req, res) => {
   } catch (error) {
     res.status(400).json({ error: error.message });
     // console.log(comicId);
-  }
-});
-
-///////////////////////////////////////////
-/////////   COMICS PART ROUTES ////////////
-///////////////////////////////////////////
-
-router.get("/characters", async (req, res) => {
-  try {
-    // Query filters:  name, limit and skip -
-    const name = req.query.name;
-    const limit = req.query.limit || 100;
-    const skip = req.query.skip || 0;
-
-    // Request Characters from API
-    const response = await axios.get(
-      `https://lereacteur-marvel-api.herokuapp.com/characters?apiKey=${process.env.MARVEL_API}&limit${limit}&skip${skip}&name${name}`
-    );
-    const characters = response.data;
-    res.status(200).json(characters);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-});
-
-router.get("/character/:characterId", async (req, res) => {
-  try {
-    // Request infos of a specific character
-    const response = await axios.get(
-      `https://lereacteur-marvel-api.herokuapp.com/character/${req.params.characterId}?apiKey=${process.env.MARVEL_API}`
-    );
-    const characterId = response.data;
-    res.status(200).json(characterId);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
   }
 });
 
